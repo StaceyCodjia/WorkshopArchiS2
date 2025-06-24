9459b38a7490a6e29bcb76541a68b63f0f32a2c7
ClaireG0702 <claire.gobert2002@gmail.com>
2025-06-24 17:42:26 +0200

    Ajout + modif etudiant - amelioration affichage

--- main.py
+++ main.py
@@ -8,20 +8,41 @@ etudiants = []
 def index():
     return render_template('index.html', etudiants=etudiants)
 
-@app.route("/add")
-def add():
-    return render_template('add.html')
+@app.route("/new")
+def new():
+    return render_template('/student/new.html')
 
 @app.route("/reponse", methods=["POST"])
 def traitement():
     nomEtud = request.form["nom"]
     prenomEtud = request.form["prenom"]
-    NumEtud = request.form["num-etud"]
+    NumEtud = request.form["num_etud"]
 
     etudiants.append({
+        "id": len(etudiants) + 1,
         "nom": nomEtud,
         "prenom": prenomEtud,
-        "num-etud": NumEtud
+        "num_etud": NumEtud
     })
+
+    return redirect(url_for('index'))
+
+@app.route("/edit/<int:id>")
+def edit(id):
+    etudiant = etudiants[id - 1]
+    return render_template('/student/edit.html', etudiant=etudiant)
+
+@app.route("/update/<int:id>", methods=["POST"])
+def update(id):
+    nomEtud = request.form["nom"]
+    prenomEtud = request.form["prenom"]
+    NumEtud = request.form["num_etud"]
+
+    etudiants[id - 1] = {
+        "id": id,
+        "nom": nomEtud,
+        "prenom": prenomEtud,
+        "num_etud": NumEtud
+    }
 
     return redirect(url_for('index'))
