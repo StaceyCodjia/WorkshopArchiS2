78258f4fabadb529b9fb32942f5e01ae24d1c468
ClaireG0702 <claire.gobert2002@gmail.com>
2025-06-26 21:41:16 +0200

    Groupe associe a un projet

--- controllers/group_controller.py
+++ controllers/group_controller.py
@@ -1,5 +1,6 @@
 from flask import jsonify, Blueprint, request, render_template
 from models.groupe import Groupe
+from models.projet import Projet
 
 group_bp = Blueprint('groupe', __name__)
 
@@ -23,6 +24,10 @@ def edit_form(id):
 def get_groups():
     groupes = Groupe.get_all()
 
+    for groupe in groupes:
+        projet = Projet.get_one(groupe.projet_id)
+        groupe.nom_projet = projet.nom_projet if projet else None
+
     groupes_dict = [vars(groupe) for groupe in groupes]
     return jsonify(groupes_dict)
 
@@ -37,19 +42,21 @@ def get_group(id):
 @group_bp.route("/api/group", methods=["POST"])
 def create_group():
     data = request.get_json()
-    nom_group = data.get("nom_group")
+    nom_groupe = data.get("nom_groupe")
     nbr_personnes = data.get("nbr_personnes")
     projet_id = data.get("projet_id")
 
-    group_id = Groupe.post(nom_group, nbr_personnes,projet_id)
+    group_id = Groupe.post(nom_groupe, nbr_personnes, projet_id)
     return jsonify({"message": "Group created successfully", "id": group_id}), 201
 
 @group_bp.route("/api/group/<int:id>/edit", methods=["PUT"])
 def update_group(id):
     data = request.get_json()
-    nom_group = data.get("nom_group")
+    nom_groupe = data.get("nom_groupe")
     nbr_personnes = data.get("nbr_personnes")
-    Groupe.put(id, nom_group, nbr_personnes)
+    projet_id = data.get("projet_id")
+
+    Groupe.put(id, nom_groupe, nbr_personnes, projet_id)
     return jsonify({"message": "Group updated successfully"})
 
 @group_bp.route("/api/group/<int:id>/delete", methods=["DELETE"])
--- models/groupe.py
+++ models/groupe.py
@@ -1,8 +1,9 @@
 from connexion import get_db_connection
 
 class Groupe:
-    def __init__(self, id, nbr_personnes, projet_id):
+    def __init__(self, id, nom_groupe, nbr_personnes, projet_id):
         self.id = id
+        self.nom_groupe = nom_groupe
         self.nbr_personnes = nbr_personnes
         self.projet_id = projet_id
 
@@ -17,7 +18,7 @@ class Groupe:
         conn.close()
 
         if groupe:
-            return Groupe(groupe['Id'], groupe['nom_group'], groupe['Nbr_Personnes'], groupe['Projet_Id'])
+            return Groupe(groupe['Id'], groupe['Nom_Groupe'], groupe['Nbr_Personnes'], groupe['Projet_Id'])
         return None
     
     def get_all():
@@ -30,14 +31,14 @@ class Groupe:
         cursor.close()
         conn.close()
 
-        return [Groupe(groupe['Id'],groupe['nom_group'], groupe['Nbr_Personnes'], groupe['Projet_Id']) for groupe in groupes]
+        return [Groupe(groupe['Id'],groupe['Nom_Groupe'], groupe['Nbr_Personnes'], groupe['Projet_Id']) for groupe in groupes]
     
-    def post(nom_group, nbr_personnes, projet_id):
+    def post(nom_groupe, nbr_personnes, projet_id):
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        query = "INSERT INTO Groupe (nom_group, Nbr_Personnes, Projet_Id) VALUES (%s,%s, %s)"
-        cursor.execute(query, (nom_group, nbr_personnes, projet_id))
+        query = "INSERT INTO Groupe (Nom_Groupe, Nbr_Personnes, Projet_Id) VALUES (%s, %s, %s)"
+        cursor.execute(query, (nom_groupe, nbr_personnes, projet_id))
         conn.commit()
 
         cursor.close()
@@ -45,12 +46,12 @@ class Groupe:
 
         return cursor.lastrowid
     
-    def put(id, nom_group, nbr_personnes):
+    def put(id, nom_groupe, nbr_personnes, projet_id):
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        query = "UPDATE Groupe SET nom_group = %s, Nbr_Personnes = %s WHERE Id = %s"
-        cursor.execute(query, (nom_group, nbr_personnes, id))
+        query = "UPDATE Groupe SET Nom_Groupe = %s, Nbr_Personnes = %s, Projet_Id = %s WHERE Id = %s"
+        cursor.execute(query, (nom_groupe, nbr_personnes, projet_id, id))
         conn.commit()
 
         cursor.close()
