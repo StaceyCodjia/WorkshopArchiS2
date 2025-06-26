4df5fb35a259074eb61e16f8a896e009bd6c3cf5
ClaireG0702 <claire.gobert2002@gmail.com>
2025-06-26 22:30:56 +0200

    Etudiant appartient a groupe

--- controllers/group_controller.py
+++ controllers/group_controller.py
@@ -1,6 +1,10 @@
 from flask import jsonify, Blueprint, request, render_template
 from models.groupe import Groupe
 from models.projet import Projet
+from models.etudiant import Etudiant
+from models.etudiant_groupe import EtudiantGroupe
+from models.etudiant_competence import EtudiantCompetence
+from models.competence import Competence
 
 group_bp = Blueprint('groupe', __name__)
 
@@ -28,7 +32,39 @@ def get_groups():
         projet = Projet.get_one(groupe.projet_id)
         groupe.nom_projet = projet.nom_projet if projet else None
 
-    groupes_dict = [vars(groupe) for groupe in groupes]
+        groupe.etudiants = EtudiantGroupe.get_all_by_groupe_id(groupe.id)
+        for etudiant in groupe.etudiants:
+            infos = Etudiant.get_one(etudiant.etudiant_id)
+            comp_ids = EtudiantCompetence.get_competence_of_etudiant(etudiant.etudiant_id)
+            etudiant.competences = []
+            for comp_id in comp_ids:
+                comp = Competence.get_one(comp_id.competence_id)
+                if comp:
+                    etudiant.competences.append({"nom": comp.nom_competence})
+            etudiant.nom = infos.nom if infos else None
+            etudiant.prenom = infos.prenom if infos else None
+            etudiant.num_etudiant = infos.num_etudiant if infos else None
+
+    # Pour sérialiser proprement, transforme chaque étudiant en dict
+    def etudiant_to_dict(etudiant):
+        return {
+            "id": etudiant.etudiant_id,
+            "nom": etudiant.nom,
+            "prenom": etudiant.prenom,
+            "num_etudiant": etudiant.num_etudiant,
+            "competences": etudiant.competences
+        }
+
+    groupes_dict = []
+    for groupe in groupes:
+        groupes_dict.append({
+            "id": groupe.id,
+            "nom_groupe": groupe.nom_groupe,
+            "nbr_personnes": groupe.nbr_personnes,
+            "nom_projet": groupe.nom_projet,
+            "etudiants": [etudiant_to_dict(e) for e in groupe.etudiants]
+        })
+
     return jsonify(groupes_dict)
 
 @group_bp.route("/api/group/<int:id>")
@@ -45,8 +81,13 @@ def create_group():
     nom_groupe = data.get("nom_groupe")
     nbr_personnes = data.get("nbr_personnes")
     projet_id = data.get("projet_id")
+    etudiants = data.get("etudiants", [])
 
     group_id = Groupe.post(nom_groupe, nbr_personnes, projet_id)
+
+    for etudiant_id in etudiants:
+        EtudiantGroupe.post(etudiant_id, group_id)
+
     return jsonify({"message": "Group created successfully", "id": group_id}), 201
 
 @group_bp.route("/api/group/<int:id>/edit", methods=["PUT"])
@@ -55,11 +96,18 @@ def update_group(id):
     nom_groupe = data.get("nom_groupe")
     nbr_personnes = data.get("nbr_personnes")
     projet_id = data.get("projet_id")
+    etudiants = data.get("etudiants", [])
 
     Groupe.put(id, nom_groupe, nbr_personnes, projet_id)
+
+    EtudiantGroupe.delete_all_by_groupe_id(id)
+    for etudiant_id in etudiants:
+        EtudiantGroupe.post(etudiant_id, id)
+
     return jsonify({"message": "Group updated successfully"})
 
 @group_bp.route("/api/group/<int:id>/delete", methods=["DELETE"])
 def delete_group(id):
+    EtudiantGroupe.delete_all_by_groupe_id(id)
     Groupe.delete(id)
     return jsonify({"message": "Group deleted successfully"}), 204
--- models/etudiant_groupe.py
+++ models/etudiant_groupe.py
@@ -1,4 +1,58 @@
+from connexion import get_db_connection
+
 class EtudiantGroupe:
     def __init__(self, etudiant_id, groupe_id):
         self.etudiant_id = etudiant_id
         self.groupe_id = groupe_id
+
+    def get_all_by_groupe_id(groupe_id):
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute("""
+        SELECT *
+        FROM Etudiant
+        JOIN Appartient ON Etudiant.Id = Appartient.Etudiant_Id
+        WHERE Appartient.Groupe_Id = %s
+        """, (groupe_id,))
+        
+        etudiants = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return [EtudiantGroupe(etudiant['Id'], groupe_id) for etudiant in etudiants]
+    
+    def post(etudiant_id, groupe_id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        query = "INSERT INTO Appartient (Etudiant_Id, Groupe_Id) VALUES (%s, %s)"
+        cursor.execute(query, (etudiant_id, groupe_id))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return cursor.lastrowid
+    
+    def put(etudiant_id, groupe_id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        query = "UPDATE Appartient SET Etudiant_Id = %s WHERE Groupe_Id = %s"
+        cursor.execute(query, (etudiant_id, groupe_id))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+    
+    def delete_all_by_groupe_id(groupe_id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("DELETE FROM Appartient WHERE Groupe_Id = %s", (groupe_id,))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
